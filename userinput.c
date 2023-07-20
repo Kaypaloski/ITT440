ed8dfc42970d1e27699f299f765044d8aa615429
Kaypaloski <140033367+Kaypaloski@users.noreply.github.com>
2023-07-20 19:46:44 +0800

    Delete userinput.c

--- userinput.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void){ //This is  my second program in C//
-	int age;
-	printf("Hi, How old are you? >");
-	scanf("%d",&age);
-	printf("You are %d years old",age);
-	return 0;
-}
